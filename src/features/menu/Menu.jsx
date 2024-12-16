b4ba65ccbbc7c4b991213b1dcbc2b02188731c66
hybridGithub <abhishekmaestro@gmail.com>
2024-12-16 13:44:40 +0530

    first commit

---
+++ src/features/menu/Menu.jsx
@@ -0,0 +1,9 @@
+const Menu = () => {
+  return (
+    <div>
+      <h2>My Menu page</h2>
+    </div>
+  )
+}
+
+export default Menu;
---
+++ src/ui/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h2>My Home page</h2>
+    </div>
+  )
+}
+
+export default Home;
