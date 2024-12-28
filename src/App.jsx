0740364087027969f29a3ad711ca1287f973520d
hybridGithub <abhishekmaestro@gmail.com>
2024-12-28 11:58:38 +0530

    Error handing in form actions

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order, { loader as orderLoader } from "./features/order/Order";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, {action as createOrderAction } from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
 
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
       { path: '/', element: <Home /> },
       { path: '/menu', element: <Menu />, loader: menuLoader, errorElement: <Error /> },
       { path: '/cart', element: <Cart /> },
-      { path: '/order/new', element: <CreateOrder /> },
+      { path: '/order/new', element: <CreateOrder />, action: createOrderAction },
       { path: '/order/:order_id', element: <Order />, loader: orderLoader, errorElement: <Error />, },
     ]
   }
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,45 @@
-import { Form } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
+
+const isValidPhone = (str) => /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(str);
+
+const fakeCart = [
+  {
+    "pizzaId": 1,
+    "name": "Margherita",
+    "quantity": 2,
+    "unitPrice": 8.99,
+    "totalPrice": 17.98
+  },
+  {
+    "pizzaId": 2,
+    "name": "Pepperoni",
+    "quantity": 1,
+    "unitPrice": 10.49,
+    "totalPrice": 10.49
+  },
+  {
+    "pizzaId": 3,
+    "name": "Vegetarian",
+    "quantity": 3,
+    "unitPrice": 9.99,
+    "totalPrice": 29.97
+  },
+  {
+    "pizzaId": 4,
+    "name": "Hawaiian",
+    "quantity": 1,
+    "unitPrice": 11.99,
+    "totalPrice": 11.99
+  }
+];
 
 const CreateOrder = () => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+  const formErrors = useActionData();
+
+  const cart = fakeCart;
 
   return (
     <div className="px-4 py-6">
@@ -13,6 +52,7 @@ const CreateOrder = () => {
         <div>
           <label>Phone</label>
           <input type="tel" name="phone" required />
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
         <div>
           <label>Address</label>
@@ -25,7 +65,8 @@ const CreateOrder = () => {
           <label htmlFor="priority">Want to give your order priority</label>
         </div>
         <div>
-          <button>Order now</button>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <button disabled={isSubmitting}>{isSubmitting ? "Placing order..." : "Order now"}</button>
         </div>
       </Form>
     </div>
@@ -34,6 +75,24 @@ const CreateOrder = () => {
 
 export async function action({ request }) {
   const formData = await request.formData();
+  
+  const data = Object.fromEntries(formData);
+
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === 'on',
+  }
+  
+  const errors = {};
+
+  if (!isValidPhone(order.phone)) errors.phone = "Please provide your correct number.";
+
+  if(Object.keys(errors).length > 0) return errors;
+
+  const newOrder = await createOrder(order);
+
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
